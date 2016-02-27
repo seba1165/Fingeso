2acdb2384171b517cfc1dcb2a7b9fb0497e1a2b3
veronicaDominguez <veronica.dominguezv@gmail.com>
2016-02-26 21:03:30 -0300

    sin :registerable!

--- sv2/app/models/empleado.rb
+++ sv2/app/models/empleado.rb
@@ -1,7 +1,7 @@
 class Empleado < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
